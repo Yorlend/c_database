a8afbf58fddf1b46bb1acb676e3b0d90e4632c0e
yorlend <yorlend@github.com>
2022-05-14 19:07:17 +0300

    pair arr struct

---
+++ src/utils/pair_array.h
@@ -0,0 +1,16 @@
+#ifndef __PAIR_ARRAY_H__
+#define __PAIR_ARRAY_H__
+
+#include "pair.h"
+#include <stddef.h>
+
+typedef struct pair_array
+{
+    pair_t* data;
+    size_t size;
+} pair_array_t;
+
+
+
+
+#endif
